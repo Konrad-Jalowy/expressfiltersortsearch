42c809e96ea18a27d4c0b8ba04757a207b343e4e
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-09 10:57:59 +0100

    were getting somewhere

--- models/seeder1.js
+++ models/seeder1.js
@@ -10,7 +10,10 @@ const fname = "./users1.json";
 (async () => {
     try {
         await mongoose.connect(process.env.MONGO_URL);
+        
         console.log("seeder running");
+        const users = JSON.parse(fs.readFileSync(`${__dirname}/users1.json`, 'utf-8'));
+        console.log(users);
     } catch (err) {
       console.log('error: ' + err)
     }
