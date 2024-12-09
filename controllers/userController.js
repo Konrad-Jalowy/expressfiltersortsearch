d23d4bf540c0f1226e0ac67eec97885576df6c16
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-09 11:33:03 +0100

    catchasync added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,13 @@
 const User = require("../models/userModel");
+const catchAsync = require("../catchAsync");
 
 exports.main = (req, res) => {
     return res.json({"msg": "main page"});
 }
 
 
-exports.byId =  async (req, res) => {
+exports.byId =  catchAsync(async (req, res, next) => {
     let _user = await User.findOne({_id: req.params.id});
     console.log(_user);
     return res.json({"user": _user});
-}
+});
