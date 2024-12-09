a0b2f91c887b4667e0fdc392093eea32f8083163
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-09 11:26:26 +0100

    skeleton added

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
+const UserRouter = require("./routes/userRoutes");
 
 
 const app = express();
+
+app.use("/", UserRouter);
 
 
 module.exports = app;
---
+++ controllers/userController.js
@@ -0,0 +1,5 @@
+
+
+exports.main = (req, res) => {
+    return res.json({"msg": "main page"});
+}
