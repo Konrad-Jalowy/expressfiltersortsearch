7d7277cbcf41629f3f3680e66f93952c0a8a09c0
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-09 10:55:20 +0100

    seeder

--- models/seeder1.js
+++ models/seeder1.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
 const mongoose = require('mongoose');
-
+const dotenv = require('dotenv').config();;
 
 const User = require("./userModel");
 const fname = "./users1.json";
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('DB connection successful!'));
+
+console.log("seeder running");
