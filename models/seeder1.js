3a8814957e92b358b85c9e5c1c8feb2d800b9fb6
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-09 11:09:15 +0100

    seeder working as expected

--- models/seeder1.js
+++ models/seeder1.js
@@ -1,33 +1,31 @@
 const fs = require('fs').promises;
 const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();;
+
+require('dotenv').config();
 
 const User = require("./userModel");
-const fname = "users1.json";
+const fname = process.argv[2] ?? "users1.json";
 
-// const importData = async (users) => {
-//     try {
-//       await User.create(users);
-//       console.log(`Data successfully created from json file ${fname}!`);
-//     } catch (err) {
-//       console.log(err);
-//     }
-//     process.exit();
-//   };
+const importData = async (users) => {
+    try {
+      await User.create(users);
+      console.log(`Data successfully created from json file ${fname}!`);
+    } catch (err) {
+      console.log(err);
+    }
+    process.exit();
+  };
  
-// (async () => {
-//     try {
-//         await mongoose.connect(process.env.MONGO_URL);
+(async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
         
-//         console.log("seeder running");
-//         const data = await fs.readFile(`${__dirname}/${fname}`, 'utf-8');
-//         const users = JSON.parse(data);
-//         importData(users);
-//     } catch (err) {
-//       console.log('error: ' + err)
-//     }
-//   })()
+        console.log("seeder running");
+        const data = await fs.readFile(`${__dirname}/${fname}`, 'utf-8');
+        const users = JSON.parse(data);
+        importData(users);
+    } catch (err) {
+      console.log('error: ' + err)
+    }
+  })()
 
-console.log(process.argv)
-const fname2 = process.argv[2] ?? "users1.json";
-console.log(fname2);
