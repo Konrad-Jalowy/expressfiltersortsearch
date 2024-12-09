d31a9bb27926f34ef4225d0a6e7159f772c810e2
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-09 11:35:31 +0100

    err handler added

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const UserRouter = require("./routes/userRoutes");
-
+const UserController = require('./controllers/userController');
 
 const app = express();
 
 app.use("/", UserRouter);
-
+app.use(UserController.errorHandler);
+// app.get('*', UserController.notFound);
 
 module.exports = app;
