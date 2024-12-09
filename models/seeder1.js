8b78cf8c5d3813bb07a395f5fa84b3794e2a55dd
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-09 11:00:19 +0100

    were getting somewhere

--- models/seeder1.js
+++ models/seeder1.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();;
 
@@ -12,7 +12,7 @@ const fname = "./users1.json";
         await mongoose.connect(process.env.MONGO_URL);
         
         console.log("seeder running");
-        const users = JSON.parse(fs.readFileSync(`${__dirname}/users1.json`, 'utf-8'));
+        const users = await fs.readFile(`${__dirname}/users1.json`, 'utf-8');
         console.log(users);
     } catch (err) {
       console.log('error: ' + err)
